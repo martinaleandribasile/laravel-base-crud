f2d5be3a333a0eafa88ca97aa31e1d85954402c8
MartinaBasile <martinaleandribasile@gmail.com>
2022-11-07 15:29:11 +0100

    comics seeder correct

--- database/seeds/comicsTableSeeder.php
+++ database/seeds/comicsTableSeeder.php
@@ -17,13 +17,13 @@ class comicsTableSeeder extends Seeder
         $comicsData = config('data.comics');
         foreach ($comicsData as $comicData) {
             $comic = new comic();
-            $comic->title = $comicData->title;
-            $comic->description = $comicData->description;
-            $comic->thumb = $comicData->thumb;
-            $comic->price = $comicData->price;
-            $comic->series = $comicData->series;
-            $comic->sale_date = $comicData->sale_date;
-            $comic->type = $comicData->type;
+            $comic->title = $comicData['title'];
+            $comic->description = $comicData['description'];
+            $comic->thumb = $comicData['thumb'];
+            $comic->price = $comicData['price'];
+            $comic->series = $comicData['series'];
+            $comic->sale_date = $comicData['sale_date'];
+            $comic->type = $comicData['type'];
             $comic->save();
         }
     }
