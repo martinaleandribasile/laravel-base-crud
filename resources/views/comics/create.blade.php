20af7599ba7e72973b5af77e53884a19bcb45759
MartinaBasile <martinaleandribasile@gmail.com>
2022-11-10 16:29:21 +0100

    add error validation on unvalid input, add style

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -5,46 +5,83 @@
 @endsection
 
 @section('contentMain')
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
     <div class="boxcontainer">
         <h1 class="text-center text-uppercase text-success">Aggiungi un nuovo Comic</h1>
         <form action="{{ route('comics.store') }}" method="POST">
             @csrf
             <div class="box-input">
                 <label for="title">Titolo:</label>
-                <input type="text" name="title" value='{{ old('title', '') }}' required />
+                <input type="text" name="title" value='{{ old('title', '') }}' required maxlength="190" minlength="1" />
+                @error('title')
+                    <div class='alert alert-danger p-1 ms-3 mb-0'>
+                        {{ __($message) }}
+                        <!-- i __ sono per aggiungere le traduzioni per le lingue-->
+                    </div>
+                @enderror
+
             </div>
             <div class="box-input">
                 <label for="description">Descrizione:</label>
-                <textarea name="description" id="" cols="30" rows="10">{{ old('description', '') }}</textarea>
+                <textarea name="description" id="" cols="30" rows="10" required minlength="1">{{ old('description', '') }}</textarea>
+                @error('description')
+                    <div class='alert alert-danger p-1 ms-3 mb-0'>
+                        {{ __($message) }}
+                        <!-- i __ sono per aggiungere le traduzioni per le lingue-->
+                    </div>
+                @enderror
             </div>
             <div class="box-input">
                 <label for="thumb">Immagine Copertina:</label>
-                <input type='text' name="thumb" value='{{ old('thumb', '') }}' />
+                <input type='url' name="thumb" value='{{ old('thumb', '') }}' required />
+                @error('thumb')
+                    <div class='alert alert-danger p-1 ms-3 mb-0'>
+                        {{ __($message) }}
+                        <!-- i __ sono per aggiungere le traduzioni per le lingue-->
+                    </div>
+                @enderror
             </div>
             <div class="box-input">
                 <label for="price">Prezzo:</label>
-                <input type="float" name="price" min="0" value='{{ old('price', '') }}' required />
+                <input type="number" step="0.01" name="price" min="0" value='{{ old('price', '') }}'
+                    required />
+                @error('price')
+                    <div class='alert alert-danger p-1 ms-3 mb-0'>
+                        {{ __($message) }}
+                        <!-- i __ sono per aggiungere le traduzioni per le lingue-->
+                    </div>
+                @enderror
             </div>
             <div class="box-input">
                 <label for="series">Serie:</label>
-                <input type="text" name="series" value='{{ old('series', '') }}' />
+                <input type="text" name="series" value='{{ old('series', '') }}'maxlength="90" minlength="1"
+                    required />
+                @error('series')
+                    <div class='alert alert-danger p-1 ms-3 mb-0'>
+                        {{ __($message) }}
+                        <!-- i __ sono per aggiungere le traduzioni per le lingue-->
+                    </div>
+                @enderror
             </div>
             <div class="box-input">
                 <label for="sale_date">Data Uscita:</label>
-                <input type="date" name="sale_date" value='{{ old('sale_date', '') }}' />
+                <input type="date" name="sale_date" value='{{ old('sale_date', '') }}' required />
+                @error('sale_date')
+                    <div class='alert alert-danger p-1 ms-3 mb-0'>
+                        {{ __($message) }}
+                        <!-- i __ sono per aggiungere le traduzioni per le lingue-->
+                    </div>
+                @enderror
             </div>
             <div class="box-input">
                 <label for="type">Tipologia:</label>
-                <input type="text" name="type" />
+                <input type="text" name="type" maxlength="90" minlength="1" required
+                    value='{{ old('type', '') }}' />
+                @error('type')
+                    <div class='alert alert-danger p-1 ms-3 mb-0'>
+                        {{ __($message) }}
+                        <!-- i __ sono per aggiungere le traduzioni per le lingue-->
+                    </div>
+                @enderror
             </div>
             <div class="box-input pt-5">
                 <input type="submit" class="btn btn-success" value="Registra">
--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -5,15 +5,6 @@
 @endsection
 
 @section('contentMain')
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
     <div class="boxcontainer">
         <h1 class="text-center text-uppercase text-success">Aggiungi un nuovo Comic</h1>
         <form action="{{ route('comics.update', $comic->id) }}" method="POST">
@@ -21,31 +12,77 @@
             @method('PUT')
             <div class="box-input">
                 <label for="title">Titolo:</label>
-                <input type="text" name="title" value="{{ old('title', $comic->title) }}">
+                <input type="text" name="title" value="{{ old('title', $comic->title) }}" required maxlength="190"
+                    minlength="1" />
+                @error('title')
+                    <div class='alert alert-danger p-1 ms-3 mb-0'>
+                        {{ __($message) }}
+                        <!-- i __ sono per aggiungere le traduzioni per le lingue-->
+                    </div>
+                @enderror
             </div>
             <div class="box-input">
                 <label for="description">Descrizione:</label>
-                <textarea name="description" id="">{{ old('description', $comic->description) }}</textarea>
+                <textarea name="description" id="" cols="30" rows="10" required minlength="1">{{ old('description', $comic->description) }}</textarea>
+                @error('description')
+                    <div class='alert alert-danger p-1 ms-3 mb-0'>
+                        {{ __($message) }}
+                        <!-- i __ sono per aggiungere le traduzioni per le lingue-->
+                    </div>
+                @enderror
             </div>
             <div class="box-input">
                 <label for="thumb">Immagine Copertina:</label>
-                <input type='text' name="thumb" value="{{ old('thumb', $comic->thumb) }}">
+                <input type="url" name="thumb" value="{{ old('thumb', $comic->thumb) }}" required>
+                @error('thumb')
+                    <div class='alert alert-danger p-1 ms-3 mb-0'>
+                        {{ __($message) }}
+                        <!-- i __ sono per aggiungere le traduzioni per le lingue-->
+                    </div>
+                @enderror
             </div>
             <div class="box-input">
                 <label for="price">Prezzo:</label>
-                <input type="float" name="price" min="0" value="{{ old('price', $comic->price) }}">
+                <input type="number" name="price" step="0.01" min="0" value="{{ old('price', $comic->price) }}"
+                    required />
+                @error('price')
+                    <div class='alert alert-danger p-1 ms-3 mb-0'>
+                        {{ __($message) }}
+                        <!-- i __ sono per aggiungere le traduzioni per le lingue-->
+                    </div>
+                @enderror
             </div>
             <div class="box-input">
                 <label for="series">Serie:</label>
-                <input type="text" name="series" value="{{ old('series', $comic->series) }}">
+                <input type="text" name="series" value="{{ old('series', $comic->series) }}" maxlength="90"
+                    minlength="1" required />
+                @error('series')
+                    <div class='alert alert-danger p-1 ms-3 mb-0'>
+                        {{ __($message) }}
+                        <!-- i __ sono per aggiungere le traduzioni per le lingue-->
+                    </div>
+                @enderror
             </div>
             <div class="box-input">
                 <label for="sale_date">Data Uscita:</label>
-                <input type="date" name="sale_date" value="{{ old('sale_date', $comic->sale_date) }}">
+                <input type="date" name="sale_date" value="{{ old('sale_date', $comic->sale_date) }}" required />
+                @error('sale_date')
+                    <div class='alert alert-danger p-1 ms-3 mb-0'>
+                        {{ __($message) }}
+                        <!-- i __ sono per aggiungere le traduzioni per le lingue-->
+                    </div>
+                @enderror
             </div>
             <div class="box-input">
                 <label for="type">Tipologia:</label>
-                <input type="text" name="type" value="{{ old('type', $comic->type) }}">
+                <input type="text" name="type" value="{{ old('type', $comic->type) }}" maxlength="90" minlength="1"
+                    required />
+                @error('type')
+                    <div class='alert alert-danger p-1 ms-3 mb-0'>
+                        {{ __($message) }}
+                        <!-- i __ sono per aggiungere le traduzioni per le lingue-->
+                    </div>
+                @enderror
             </div>
             <div class="box-input pt-5">
                 <input type="submit" class="btn btn-success" value="Aggiorna">
